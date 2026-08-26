794b0289b02c477136900d020342a8674e158540
ngarre <a28710@svalero.com>
2026-08-26 12:10:20 +0200

    feat: add reactive product state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Producto } from './producto/producto'; // Here we make TS recognise the symbol "Product"
 import { ProductoModel } from './models/producto.model';
@@ -21,18 +21,27 @@ export class App {
 
   // With signal "productos" doesn't contain directly the array --> now it has a reactive container which contains the array
   // With readonly we will not reassign the value of the signal, but we can update its content (the array)
-  readonly productos = signal<ProductoModel[]>([ 
+  readonly productos = signal<ProductoModel[]>([
     { id: 2, nombre: 'Centrífuga', precio: 2000 },
     { id: 3, nombre: 'Espectrofotómetro', precio: 3500 },
     { id: 4, nombre: 'Pipeta', precio: 200 },
   ]);
 
-
+  // property numeroProductos is readonly because it can't be reassigned to another signal --> computed internally uses ComputedSignal<number>
+  readonly numeroProductos = computed(() => this.productos().length);
 
   eliminarProducto(id: number): void {
     // console.log('Producto a eliminar: ', id);
     // Updating array of products with those ones which button hasn't been clicked
-    this.productos.update(productos => productos.filter(producto => producto.id !== id)); 
+    this.productos.update(productos => productos.filter(producto => producto.id !== id));
     // We access the array containing the "products" signal using the "update" method
+  }
+
+  restablecerProductos(): void {
+    this.productos.set([
+      { id: 2, nombre: 'Centrífuga', precio: 2000 },
+      { id: 3, nombre: 'Espectrofotómetro', precio: 3500 },
+      { id: 4, nombre: 'Pipeta', precio: 200 },
+    ]);
   }
 }
