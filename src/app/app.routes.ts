cb47b001e3016c8e15b62afbf08fc2dc3cd9ee78
ngarre <a28710@svalero.com>
2026-08-27 17:26:52 +0200

    feat: add products route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListaProductos } from './lista-productos/lista-productos';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'productos',
+    component: ListaProductos
+}];
