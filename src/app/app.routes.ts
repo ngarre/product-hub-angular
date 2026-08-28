32494dbe8a74255428c2abdaa38a211fd7ec66aa
ngarre <a28710@svalero.com>
2026-08-28 11:30:56 +0200

    feat: add product navigation routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { ListaProductos } from './lista-productos/lista-productos';
+import { FormularioProducto } from './formulario-producto/formulario-producto';
 
 export const routes: Routes = [
     {
+        path: '', // When the URL is empty...
+        redirectTo: 'productos', // ... redirect to "http://localhost:4200/productos" 
+        pathMatch: 'full' // Only if the URL is exactly: http://localhost:4200/  --> Do not accept partial matches
+    },
+    {
         path: 'productos',
         component: ListaProductos
+    },
+    {
+        path: 'nuevo-producto',
+        component: FormularioProducto
+    },
+    {
+        path: '**', // For any path which doesn't match with any of the previous ones --> this code must be at the end of the list of routes
+        redirectTo: 'productos'
     }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormularioProducto } from './formulario-producto/formulario-producto';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormularioProducto], // making Angular can use product component in the app template
+  imports: [RouterOutlet, RouterLink], // making Angular can use product component in the app template
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
